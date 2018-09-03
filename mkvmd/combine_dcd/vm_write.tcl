3363d280cb86d6831d5a25fabb08bdb4916a66b1
tabrischunchan <tabrischunchan@gmail.com>
2018-09-03 17:22:26 -0400

    modified vm_write.tcl

--- mkvmd/combine_dcd/vm_write.tcl
+++ mkvmd/combine_dcd/vm_write.tcl
@@ -1,4 +1,4 @@
-set seltext {protein or segname "L.*" "W." ION or {segname "WT.*" and name OH2 and within 10 of protein}}
+set seltext {protein or segname "L.*" "W." MEMB ION or {segname "WT.*" and name OH2 and within 10 of protein}}
 set outname "noW"
 
 mol new ../../../ionized.psf
