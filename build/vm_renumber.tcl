0d41d792b99f370bc91af2ce2b1d8676a25694c6
tabrischunchan <tabrischunchan@gmail.com>
2019-03-08 12:01:22 -0500

    Updated vm_renumber.tcl

--- build/vm_renumber.tcl
+++ build/vm_renumber.tcl
@@ -1,14 +1,11 @@
-mol new ../combine_dcd/initial.psf
-mol addfile ../combine_dcd/initial.pdb
-set sel1 [atomselect top all]
-$sel1 set segname L1
-$sel1 set chain L
-set oldNumbering [$sel1 get residue]
+mol new .psf
+mol addfile .pdb
+set sel [atomselect top all]
+set oldNumbering [$sel get residue]
 set newNumbering {}
 foreach resid $oldNumbering {
     lappend newNumbering [expr {$resid + 1}]
 }
-$sel1 set resid $newNumbering
+$sel set resid $newNumbering
 
-$sel1 writepdb renumbered.pdb
-$sel1 writepsf renumbered.psf
+$sel writepdb renumbered.pdb
