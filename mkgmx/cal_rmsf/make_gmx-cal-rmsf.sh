f3a99877c436cb5124d35f7216a46bacc3f90c0a
tabrischunchan <tabrischunchan@gmail.com>
2019-04-05 10:32:53 -0400

    Routine

--- mkgmx/cal_rmsf/make_gmx-cal-rmsf.sh
+++
@@ -1,8 +0,0 @@
-GMXPATH=
-
-initial_gro=../../../system.gro
-input_xtc=../combine_trr/md.xtc
-index=../../index.ndx
-begin_ps=400
-
-gmx_mpi rmsf -s $initial_gro -f $input_xtc -n $index -b $begin_ps -res
--- mkgmx/combine_trr/make_pdb.sh
+++
@@ -1,5 +0,0 @@
-GMX=/home/kevin/opt/gromacs-5.1.1-MPI-CUDA-single/bin/gmx_mpi
-
-filename=../../../system.gro
-
-$GMX editconf -f $filename -o initial.pdb -n ../../index.ndx
