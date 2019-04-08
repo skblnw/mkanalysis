5cc4770fc7f32182d61118c4c6a7f7a8c2f48dc6
tabrischunchan <tabrischunchan@gmail.com>
2019-04-07 23:09:49 -0400

    Added covar

---
+++ mkpy/covar/sklearn/cov_sparse.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+print(__doc__)
+# author: Gael Varoquaux <gael.varoquaux@inria.fr>
+# License: BSD 3 clause
+# Copyright: INRIA
+
+import numpy as np
+from scipy import linalg
+from sklearn.datasets import make_sparse_spd_matrix
+from sklearn.covariance import GraphLassoCV, ledoit_wolf
+import matplotlib.pyplot as plt
+import mdtraj as md
+
+# #############################################################################
+# Generate the data
+n_samples = 5
+n_features = 4
+
+traj = md.load('/home/PHARMACY/chan.773/storage/cpf1/run_rna/md-1-80-pf100ps-fit.xtc', top='/home/PHARMACY/chan.773/Dropbox/QWD/CRISPR-Cpf1/cpf1/run_rna/output/initial.pdb')
+traj.superpose(traj, 0)
+
+top = traj.topology
+sel = top.select('name CA')
+sel_traj = traj.xyz[:, sel, :]
+
+X = sel_traj.reshape(traj.n_frames, sel.shape[0] * 3)
+X -= X.mean(axis=0)
+X /= X.std(axis=0)
+
+emp_cov = np.dot(X.T, X)
+
+# #############################################################################
+# Plot the results
+plt.figure(figsize=(10, 6))
+plt.subplots_adjust(left=0.02, right=0.98)
+
+# plot the covariances
+covs = [('Empirical', emp_cov)]
+vmax = emp_cov.max()
+for i, (name, this_cov) in enumerate(covs):
+    plt.subplot(2, 4, i + 1)
+    plt.imshow(this_cov, interpolation='nearest', vmin=-vmax, vmax=vmax,
+               cmap=plt.cm.RdBu_r)
+    plt.xticks(())
+    plt.yticks(())
+    plt.title('%s covariance' % name)
