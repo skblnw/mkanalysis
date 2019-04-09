dc05535934d9abaeb96a40a47869d71bdcf0ef8c
tabrischunchan <tabrischunchan@gmail.com>
2019-04-09 17:45:13 -0400

    Added covar

--- mkpy/covar/gc/construct_gc.py
+++ mkpy/covar/gc/construct_gc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 f = open('correl.dat', 'r')
 firstline = f.readline()
@@ -10,4 +11,12 @@ data = [data for xx in data_raw for data in xx]
 
 npa = np.array(data, dtype='float')
 new = npa.reshape(size,size)
-np.savetxt(new, 'out')
+#np.savetxt(new, 'out')
+
+fig, ax = plt.subplots()
+plt.imshow(new, interpolation='gaussian', vmin=0, vmax=1,
+           cmap=plt.cm.RdBu_r)
+ax.set_xlim(1,169)
+ax.set_ylim(1,169)
+plt.colorbar()
+plt.show()
---
+++ mkpy/covar/gc/run_gc.sh
@@ -0,0 +1,4 @@
+~/opt/bin/g_correlation -linear -f /data1/kevin/Ups1-Mdm35/Gromacs/Ups1_gmx_t1/xtc/prot-1-8-pf10ps-1006ns-fit-50000.xtc -s /data1/kevin/Ups1-Mdm35/Gromacs/Ups1_gmx_t1/pdb2gmx/prot.pdb -n /data1/kevin/Ups1-Mdm35/Gromacs/Ups1_gmx_t1/pdb2gmx/index.ndx << EOF
+3
+3
+EOF
