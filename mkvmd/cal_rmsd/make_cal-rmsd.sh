e298bc562c39bb152d7c30f0e3c0eed2e7b0bdf0
tabrischunchan <tabrischunchan@gmail.com>
2019-05-28 13:51:13 -0400

    Routine

--- mkvmd/cal_rmsd/make_cal-rmsd.sh
+++ mkvmd/cal_rmsd/make_cal-rmsd.sh
@@ -3,20 +3,20 @@
 # First frame of the trajectory will be the reference structure
 # i.e. if pdb!==0, ref=<pdb>; else ref=<first frame of dcd>
 
-PSF=../../pdb2gmx/prot.pdb
+PSF=ionized.pdb
 PDB=0
-DCD=/home/PHARMACY/chan.773/storage/cpf1/run_noDNA/md-1-80-pf100ps-fit.xtc
-SELREF=("protein and resid 940 to 990")
-SELRMSD=("protein and resid 790 to 860")
-OUTPUT_NAME=("r790-860")
+DCD=(".xtc" ".xtc")
 
-for ii in {0..0}
+SELREF=("protein and name CA")
+SELRMSD=("protein and name CA")
+OUTPUT_NAME=("out")
+
+for ii in {0..1}
 do
-    ref=${SELREF[$ii]}
-    rmsd=${SELRMSD[$ii]}
-    outname=${OUTPUT_NAME[$ii]}
+    ref=${SELREF[0]}
+    rmsd=${SELRMSD[0]}
     sed -e "s/SELREF/$ref/g" \
         -e "s/SELRMSD/$rmsd/g" \
         template-vm-cal-rmsd-tcl > vm_cal-rmsd.tcl
-    vmd -dispdev text -e vm_cal-rmsd.tcl -args $PSF $PDB $DCD $outname
+    vmd -dispdev text -e vm_cal-rmsd.tcl -args $PSF $PDB ${DCD[$ii]} ${OUTPUT_NAME}-$ii
 done
