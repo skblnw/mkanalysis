42f3a89788cc77520d485210f28e150be42e6b60
tabrischunchan <tabrischunchan@gmail.com>
2019-08-20 16:31:23 -0400

    Routine

--- build/modeller/step2-automodel.py
+++ build/modeller/step2-automodel.py
@@ -8,10 +8,10 @@ env = environ()
 # directories for input atom files
 env.io.atom_files_directory = ['.', '../atom_files']
 
-a = MyModel(env,
-				alnfile  = 'alignment.seq',
-				knowns   = 'chainA',
-				sequence = 'chainA_fill')
+a = automodel(env,
+    alnfile  = 'alignment.seq',
+    knowns   = 'prot',
+    sequence = 'prot_fill')
 a.starting_model= 1
 a.ending_model  = 1
 
