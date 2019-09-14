dfa60785f4564c2977dce8d1b951e88f5ede2dc2
tabrischunchan <tabrischunchan@gmail.com>
2019-09-14 19:33:01 -0400

    Routine

--- VMD/vm_renumber.tcl
+++ VMD/vm_renumber.tcl
@@ -1,11 +1,13 @@
-mol new .psf
-mol addfile .pdb
-set sel [atomselect top all]
-set oldNumbering [$sel get residue]
-set newNumbering {}
-foreach resid $oldNumbering {
-    lappend newNumbering [expr {$resid + 1}]
-}
-$sel set resid $newNumbering
+foreach ii {A B C D} {
+    mol new $ii.pdb
+    set sel [atomselect top all]
+    set oldNumbering [$sel get residue]
+    set newNumbering {}
+    foreach resid $oldNumbering {
+        lappend newNumbering [expr {$resid + 1}]
+    }
+    $sel set resid $newNumbering
 
-$sel writepdb renumbered.pdb
+    $sel writepdb $ii.pdb
+}
+
