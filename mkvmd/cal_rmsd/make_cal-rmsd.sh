1c83c99b2cbc452e6de8007f43c4a8fcff52f6d1
tabrischunchan <tabrischunchan@gmail.com>
2019-09-16 20:04:24 -0400

    Updated rmsd and rmsf

--- mkvmd/cal_rmsd/make_cal-rmsd.sh
+++ mkvmd/cal_rmsd/make_cal-rmsd.sh
@@ -3,20 +3,66 @@
 # First frame of the trajectory will be the reference structure
 # i.e. if pdb!==0, ref=<pdb>; else ref=<first frame of dcd>
 
-PSF=ionized.pdb
-PDB=0
-DCD=(".xtc" ".xtc")
+PSF=initial-noW.psf
+TRJ=md.trr
 
-SELREF=("protein and name CA")
-SELRMSD=("protein and name CA")
-OUTPUT_NAME=("out")
+if [ ! -f $TRJ ]; then
+    echo -e "$TRJ \nTrajectory not found!"
+    exit 0
+fi
 
-for ii in {0..0}
+SELREF=("protein and name CA and not resid 420 to 448 640 to 655 1151 to 1167" \
+    "protein and name CA and resid 24 to 338" \
+    "protein and name CA and resid 339 to 590 and not resid 420 to 448 " \
+    "protein and name CA and resid 1 to 23 591 to 661 762 to 891 and not resid 640 to 655" \
+    "protein and name CA and resid 662 to 761" \
+    "protein and name CA and resid 892 to 1077 1255 to 1300" \
+    "protein and name CA and resid 1078 to 1254 and not resid 1151 to 1167")
+SELRMSD=("protein and name CA and not resid 420 to 448 640 to 655 1151 to 1167" \
+    "protein and name CA and resid 24 to 338" \
+    "protein and name CA and resid 339 to 590 and not resid 420 to 448 " \
+    "protein and name CA and resid 1 to 23 591 to 661 762 to 891 and not resid 640 to 655" \
+    "protein and name CA and resid 662 to 761" \
+    "protein and name CA and resid 892 to 1077 1255 to 1300" \
+    "protein and name CA and resid 1078 to 1254 and not resid 1151 to 1167")
+OUTPUT_NAME=("all" "rec1" "rec2" "wed" "pi" "ruvc" "nuc")
+
+echo "" > vm_cal-rmsd.tcl
+cat >> vm_cal-rmsd.tcl << EOF
+
+set mol [mol new $PSF waitfor all]
+mol addfile $TRJ waitfor all
+set num_frames [molinfo \$mol get numframes]
+set sel_all [atomselect top all]
+
+EOF
+
+for ii in {0..6}
 do
-    ref=${SELREF[0]}
-    rmsd=${SELRMSD[0]}
-    sed -e "s/SELREF/$ref/g" \
-        -e "s/SELRMSD/$rmsd/g" \
-        template-vm-cal-rmsd-tcl > vm_cal-rmsd.tcl
-    vmd -dispdev text -e vm_cal-rmsd.tcl -args $PSF $PDB ${DCD[$ii]} ${OUTPUT_NAME}-$ii
+    
+    cat >> vm_cal-rmsd.tcl << EOF
+set sel_ref0 [atomselect top "${SELREF[$ii]}" frame 0]
+set sel_ref [atomselect top "${SELREF[$ii]}"]
+set sel_rmsd0 [atomselect top "${SELRMSD[$ii]}" frame 0]
+set sel_rmsd [atomselect top "${SELRMSD[$ii]}"]
+set outfile [open "rmsd_${OUTPUT_NAME[$ii]}.dat" "w"]
+for {set i 0} {\$i<\$num_frames} {incr i} {
+    \$sel_all frame \$i
+    \$sel_ref frame \$i
+    \$sel_rmsd frame \$i
+    \$sel_all move [measure fit \$sel_ref \$sel_ref0]
+    set rmsd [measure rmsd \$sel_rmsd \$sel_rmsd0]
+    puts \$outfile "\$i \t \$rmsd"
+}
+close \$outfile
+
+
+EOF
+
 done
+
+cat >> vm_cal-rmsd.tcl << EOF
+quit
+EOF
+
+vmd -dispdev text -e vm_cal-rmsd.tcl 
--- mkvmd/cal_rmsf/make_cal-rmsf.sh
+++ mkvmd/cal_rmsf/make_cal-rmsf.sh
@@ -3,20 +3,97 @@
 # First frame of the trajectory will be the reference structure
 # i.e. if pdb!==0, ref=<pdb>; else ref=<first frame of dcd>
 
-PSF=prot.pdb
-PDB=0
-DCD=(".xtc" ".xtc")
+BLOCK_RMSF=false
 
-start_ns=0
-psperframe=100
-start_frame=$(($start_ns*1000/$psperframe))
+PSF=initial-noW.psf
+PDB=initial-noW.pdb
+TRJ=md.trr
+
+if [ ! -f $TRJ ]; then
+    echo -e "$TRJ \nTrajectory not found!"
+    exit 0
+fi
+
 
 OUTPUT_NAME=("out")
 REFSEL=("protein and name CA")
 
-for ii in {0..0}
-do
-    sel1=${REFSEL[0]}
-    sed -e "s/REFSEL/$sel1/g" template-vm-cal-rmsf-tcl > vm_cal-rmsf.tcl
-    vmd -dispdev text -e vm_cal-rmsf.tcl -args $PSF $PDB ${DCD[$ii]} $start_frame ${OUTPUT_NAME}-$ii
-done
+echo "" > vm_cal-rmsf.tcl
+cat >> vm_cal-rmsf.tcl << EOF
+
+set mol [mol new $PSF waitfor all]
+mol addfile $PDB waitfor all
+mol addfile $TRJ waitfor all
+set num_frames [molinfo \$mol get numframes]
+set seltext "$REFSEL"
+set a [atomselect top \$seltext]
+set ref [atomselect top \$seltext frame 0]
+for {set ii 0} {\$ii < \$num_frames} {incr ii} {
+    \$a frame \$ii
+    \$a move [measure fit \$a \$ref]
+}
+
+EOF
+
+
+
+if $BLOCK_RMSF; then
+
+### Block RMSF starts here ###
+
+    total_ns=2400
+    psperframe=1200
+    block_ns=240
+    block_frame=$(($block_ns*1000/$psperframe))
+    block_num=$(($total_ns/$block_ns))
+
+    for ii in $(seq 0 $(($block_num-1)))
+    do
+        start_frame=$(($block_frame*$ii+1))
+        stop_frame=$(($start_frame+$block_frame-1))
+        echo $start_frame 
+        echo $stop_frame
+
+        cat >> vm_cal-rmsf.tcl << EOF
+
+set outfile [open "rmsf_${OUTPUT_NAME}_$ii.dat" "w"]
+set seltext "protein and name CA"
+set sel [atomselect top \$seltext]
+set rmsf [measure rmsf \$sel first $start_frame last $stop_frame]
+for {set i 0} {\$i <  [\$sel num] } {incr i} {
+    puts \$outfile "[expr {\$i+1}] [lindex \$rmsf \$i]"
+}
+close \$outfile
+
+EOF
+    done
+
+    cat >> vm_cal-rmsf.tcl << EOF
+    quit
+EOF
+
+
+
+else
+
+### Ordinary RMSF starts here ###
+
+    start_frame=0
+
+    cat >> vm_cal-rmsf.tcl << EOF
+
+set outfile [open "rmsf_$OUTPUT_NAME.dat" "w"]
+set seltext "protein and name CA"
+set sel [atomselect top \$seltext]
+set rmsf [measure rmsf \$sel first $start_frame last -1]
+for {set i 0} {\$i <  [\$sel num] } {incr i} {
+    puts \$outfile "[expr {\$i+1}] [lindex \$rmsf \$i]"
+}
+close \$outfile
+
+quit
+EOF
+
+fi
+
+vmd -dispdev text -e vm_cal-rmsf.tcl 
