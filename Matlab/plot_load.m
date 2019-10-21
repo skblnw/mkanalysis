5d1c282e1095fe23990612a69ae9dc5ac5d4ace9
tabrischunchan <tabrischunchan@gmail.com>
2019-10-21 16:30:50 -0400

    Routine

---
+++ Matlab/plot_load.m
@@ -0,0 +1,25 @@
+clear;
+clc;
+
+filename = {'.dat' '.dat'};
+figure(1);clf;
+for ii=1:2
+    data=load(filename{ii});
+    hold on;
+    plot(data(:,1),data(:,2), 'linewidth',2);
+end
+ 
+legend({'' ''}, 'fontsize',28,'location','best');
+title('', 'fontsize',28);
+xlabel('', 'fontsize',28);
+ylabel('', 'fontsize',28);
+
+ax=gca;
+ax.FontSize=28;
+% xlim([0 100]);
+% ax.XTick=[0:50:100];
+% ylim([0 100]);
+% ax.YTick=[0:50:100];
+ 
+print('PLOT_','-dpng');
+
