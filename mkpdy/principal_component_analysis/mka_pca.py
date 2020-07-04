5b5f982c2cff1015af26de5881aa4868cb6928ad
Kevin <cchan2242-c@my.cityu.edu.hk>
2020-07-04 19:09:56 -0400

    Plot scripts

--- mkpdy/principal_component_analysis/mka_pca.py
+++ mkpdy/principal_component_analysis/mka_pca.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import mdtraj as md
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
@@ -9,7 +8,7 @@ traj.superpose(traj, 0)
 reduced_cartesian = pca1.fit_transform(traj.xyz.reshape(traj.n_frames, traj.n_atoms * 3))
 
 plt.figure()
-plt.scatter(reduced_cartesian[:, 0], reduced_cartesian[:,1], marker='x', c=traj.time)
+plt.scatter(reduced_cartesian[:,0], reduced_cartesian[:,1], marker='x', c=traj.time)
 plt.xlabel('PC1')
 plt.ylabel('PC2')
 plt.title('Cartesian coordinate PCA')
