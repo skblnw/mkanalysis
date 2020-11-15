d6b91854387c0c928858978fe3f401dae69d7b81
Kevin <cchan2242-c@my.cityu.edu.hk>
2020-11-14 20:27:13 -0500

    Modeller

--- build/modeller/step2-automodel-multichain.py
+++ build/modeller/step2-automodel-multichain.py
@@ -6,23 +6,19 @@ log.verbose()
 class MyModel(automodel):
     def special_patches(self, aln):
         # Rename both chains and renumber the residues in each
-        self.rename_segments(segment_ids=['A', 'B'],
-                             renumber_residues=[1, 1])
-        # # Another way to label individual chains:
-        # self.chains[0].name = 'A'
-        # self.chains[1].name = 'B'
-		# self.chains[2].name = 'C'
-
+        self.rename_segments(segment_ids=['K', 'L', 'M', 'N'])
 env = environ()
 
 # directories for input atom files
 env.io.atom_files_directory = ['.', '../atom_files']
 
-a = MyModel(env,
-				alnfile  = 'alignment.ali',
-				knowns   = 'chainA',
-				sequence = 'chainA_fill')
+a = automodel(env,
+    alnfile  = 'prot.seq',
+    knowns   = 'prot',
+    sequence = 'prot_fill',
+    assess_methods=(assess.DOPE,
+                    assess.GA341))
 a.starting_model= 1
-a.ending_model  = 1
+a.ending_model  = 3
 
 a.make()
