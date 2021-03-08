726dc310565d7223395cdc9d1d207c37e777f3f9
skblnw <i@skblnw.com>
2021-03-08 17:35:56 +0800

    Routine

--- build/modeller/step2-automodel.py
+++ build/modeller/step2-automodel.py
@@ -8,13 +8,18 @@ env = Environ()
 # directories for input atom files
 env.io.atom_files_directory = ['.', '../atom_files']
 
-a = automodel(env,
+class MyModel(AutoModel):
+    def special_patches(self, aln):
+        self.rename_segments(segment_ids=['A'],
+                             renumber_residues=[69])
+
+a = MyModel(env,
     alnfile  = 'prot.seq',
     knowns   = 'prot',
     sequence = 'prot_fill',
     assess_methods=(assess.DOPE,
                     assess.GA341))
 a.starting_model= 1
-a.ending_model  = 1
+a.ending_model  = 3
 
 a.make()
